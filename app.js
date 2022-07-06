331ffbd15fb0d54229d055f508f27547dea2ca4a
sw9504 <sladewilson9504@gmail.com>
2022-07-06 19:26:21 -0300

    Add Style and createGrid function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("All is ok.");
+const gridSize = 16;
+
+function createGrid () {
+    const divContainer = document.getElementById('div-container');
+    
+    for (let i = 0; i<gridSize**2; i++){
+        const divAddition = document.createElement('div');
+        divAddition.className = "square"
+        divContainer.appendChild(divAddition);
+    }
+}
+
+createGrid();
