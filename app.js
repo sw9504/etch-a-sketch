e11f5f9364249bb20d900577fd843af4af97c02c
sw9504 <sladewilson9504@gmail.com>
2022-07-06 21:37:35 -0300

    Add coding for grid resizing

--- app.js
+++ app.js
@@ -14,25 +14,36 @@ function getRandomInt (){
     return Math.floor(Math.random() * 255);
 }
 
+function eventSquare () {
+    const container = Array.from(document.querySelectorAll('.square'));
+
+    container.forEach(square => {
+        square.addEventListener("mouseenter", event => {
+            let rgbColor = `rgb(${getRandomInt()}, ${getRandomInt()}, ${getRandomInt()})`
+            event.target.style.backgroundColor = rgbColor;
+        });
+    });
+}
+
+function eventBtn (){
+    const btn = document.getElementById("grid-btn");
+    btn.addEventListener("click",event => {
+    const input = document.querySelector('input').value;
+    gridSize = input;
+    clearGrid();
+    });
+}
+
 function clearGrid (){
     const container = document.getElementById('div-container');
+
+    while (container.lastElementChild) 
+        container.removeChild(container.lastElementChild);
+
+    createGrid();
+    eventSquare();
 }
 
 createGrid();
-
-const container = Array.from(document.querySelectorAll('.square'));
-
-container.forEach(square => {
-    square.addEventListener("mouseenter", event => {
-        let rgbColor = `rgb(${getRandomInt()}, ${getRandomInt()}, ${getRandomInt()})`
-        event.target.style.backgroundColor = rgbColor;
-    });
-});
-
-const btn = document.getElementById("grid-btn");
-btn.addEventListener("click",event => {
-    const input = document.querySelector('input').value;
-    gridSize = input;
-    console.log(gridSize);
-});
-
+eventSquare();
+eventBtn();
